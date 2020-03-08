aa2e24411894ac5a5466a06ffbee163fb0f39fec
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-03-08 21:48:33 +0100

    Updates

---
+++ precache-manifest.4711d3ff6f504265b6a84573caef3370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8903a21d5c41811d672d6134f5e1852",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "efcceebc75374bc0cc7c",
+    "url": "/react-portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d2ce11dd2c4c0d1ab571",
+    "url": "/react-portfolio/static/css/main.89beda8c.chunk.css"
+  },
+  {
+    "revision": "efcceebc75374bc0cc7c",
+    "url": "/react-portfolio/static/js/2.522acdb1.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-portfolio/static/js/2.522acdb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ce11dd2c4c0d1ab571",
+    "url": "/react-portfolio/static/js/main.0f4a3deb.chunk.js"
+  },
+  {
+    "revision": "f18044b62ef3940397a4",
+    "url": "/react-portfolio/static/js/runtime-main.3b49c3e9.js"
+  }
+]);
